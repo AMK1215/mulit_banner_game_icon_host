7ee89ede48abc14a3b1bdac0de6ed967768dfebd
AMK1215 <aungmyokhaing1215@gmail.com>
2024-12-06 12:19:51 +0630

    updated a

--- resources/views/admin/game_list/edit.blade.php
+++ resources/views/admin/game_list/edit.blade.php
@@ -9,7 +9,7 @@
     <div class="row">
         <div class="col-12">
             <div class="container mb-3">
-                <a class="btn btn-icon btn-2 btn-primary float-end me-5" href="{{ route('admin.game_list.index') }}">
+                <a class="btn btn-icon btn-2 btn-primary float-end me-5" href="{{ route('game_list.index') }}">
                     <span class="btn-inner--icon mt-1"><i class="material-icons">arrow_back</i>Back</span>
                 </a>
             </div>
